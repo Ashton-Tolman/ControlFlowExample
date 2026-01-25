f526d18f4d6134c4d72405d6bcdf76906c1c2cf2
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-01-25 14:02:36 -0700

    add is else if example

--- ControlFlowExample/Program.cs
+++ ControlFlowExample/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*Ashton tolman
  * Spring 2026
- * SimpleCalculator
+ * control flow example
  * 
  */
 namespace ControlFlowExample
@@ -9,7 +9,40 @@ namespace ControlFlowExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            int score = 85;
+            if (score >= 90)
+            {
+                Console.WriteLine("Excellent! You got an A.");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("Good job! You passed.");
+            }
+            else
+            {
+                Console.WriteLine("Keep trying! Practice makes perfect.");
+            }
+            */
+
+            int day = 3;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("It's Monday. Time to start the week!");
+                    break;
+                case 2:
+                    Console.WriteLine("It's Tuesday. Keep going!");
+                    break;
+                case 3:
+                    Console.WriteLine("It's Wednesday. Halfway there!");
+                    break;
+                default:
+                    Console.WriteLine("It's another day. Make it count!");
+                    break;
+
+            }
+            Console.Read();
         }
     }
 }
